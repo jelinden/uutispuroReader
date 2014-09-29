e30de012b738d9e1a6f9fa3806b69db9ed65e3db
Jouni Linden <jouni.linden@gmail.com>
2014-09-29 21:16:11 +0300

    fixed production start script

--- start-prod.sh
+++
@@ -1,1 +0,0 @@
-export GOMAXPROCS=4;go build webServer.go && ./webServer -address 192.168.0.5:28517
---
+++ start-uutispuroReader.sh
@@ -0,0 +1,1 @@
+export GOMAXPROCS=4 && nohup ./webServer -address localhost:28517 > uutispuroReader.log 2>&1&
