259c1318ff33b4670dea73c456632434efd6b0ad
Jouni Linden <jouni.linden@gmail.com>
2014-10-13 20:07:54 +0300

    analytics tag

--- uutispuro.js
+++ uutispuro.js
@@ -55,7 +55,6 @@ function openWebSocket() {
                 items.push("</ul>");
             }
         });
-
         if (!$.isEmptyObject(items)) {
             $('#news-container ul').removeClass("mint");
             $ul.prepend(items.join(""));
@@ -103,3 +102,10 @@ $(function() {
         }
     })    
 });
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-2171875-4', 'auto');
+ga('send', 'pageview');
