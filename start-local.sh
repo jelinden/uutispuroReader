111d265047e0b40660ed070da25b716b2381f40a
Jouni Linden <jouni.linden@gmail.com>
2015-02-08 17:08:29 +0200

    minified js and css files

--- start-local.sh
+++ start-local.sh
@@ -1,1 +1,7 @@
-jsx --extension jsx js/ js/ && go build && ./uutispuroReader
+jsx --release --minify --extension jsx js/ js/ && 
+yuicompressor js/uutispuro-19.js > js/uutispuro-19.min.js &&
+yuicompressor styles/uutispuro-20.css > styles/uutispuro-20.min.css &&
+yuicompressor styles/uutispuro-m-20.css > styles/uutispuro-m-20.min.css &&
+yuicompressor styles/uutispuro-small-20.css > styles/uutispuro-small-20.min.css &&
+go build && 
+./uutispuroReader
